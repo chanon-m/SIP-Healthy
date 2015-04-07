f94d5eb8d0660998628bee451438afe9b54bd261
Chanon <chanon_m@hotmail.com>
2015-04-06 23:14:14 -0700

    Create sip_healthy.pl

---
+++ sip_healthy.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Net::SIP;
+
+my $current = `date +"%m-%d-%Y %T"`;
+my $registrar = "98.158.144.27";
+my $username = "8188025205";
+my $password = "85006249";
+my $status;
+my $message;
+
+my $nmap = `/usr/bin/nmap -T5 -P0 -sU -p5060 $registrar`;
+chomp($nmap);
+if(index($nmap,"open|filtered") == -1) {
+  $status = 1;
+  $message = "Closed";
+} else {
+  $status = 0;
+  $message = "Open";
+}
+
+print "$status SIP_Service status=$status; Server is $registrar, SIP Service is $message, $current";
+
+my $ua = Net::SIP::Simple->new(
+registrar => $registrar,
+domain => $registrar,
+from => $username,
+auth => [ $username,$password ],
+
+);
+
+# Register agent
+if($ua->register( expires => 1800 )) {
+  $status = 0;
+  $message = "UP";
+}else {
+  $status = 2;
+  $message = "DOWN";
+}
+
+print "$status SIP_Register status=$status; Server is $registrar, $current\n";
